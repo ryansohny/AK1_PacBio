e195bc246c2f7f73eac3cd5ebab21f509553431a
ryansohny <phenomata@gmail.com>
2022-08-18 16:51:06 +0900

    Added _02.aligned_bam_to_cpg_scores.sh

---
+++ 01.DNA/_02.aligned_bam_to_cpg_scores.sh
@@ -0,0 +1,28 @@
+export PATH=/mnt/mone/Project/WC300/Tools/Anaconda3/bin:$PATH
+source activate pbcpg
+python=/mnt/mone/Project/WC300/Tools/Anaconda3/envs/pbcpg/bin/python
+bamtocpg=/mnt/mone/Project/AK1_PacBio/Tools/pb-CpG-tools/aligned_bam_to_cpg_scores.py
+reference=/mnt/mone/Project/AK1_PacBio/Tools/Reference/GRCh38/DNA/hg38.analysisSet.fa
+refmmi=/mnt/mone/Project/AK1_PacBio/Tools/Reference/GRCh38/DNA/hg38.analysisSet.fa.mmi
+
+for((i=$2;i<=$3;i++))
+do
+        sed -n ${i}p $1 > tmp${i}
+        sample=$(awk '{print $1}' tmp${i})
+        bam=$(awk '{print $3}' tmp${i})
+        rm tmp${i}
+
+/usr/bin/time \
+-f "%E" \
+-o ./logs/time_cpg.out \
+${python} ${bamtocpg} \
+--bam ${bam} \
+--fasta ${reference} \
+--output_label ${sample} \
+--pileup_mode count \
+--modsites denovo \
+--min_coverage 5 \
+--min_mapq 0 \
+--hap_tag HP \
+--threads 55
+done
