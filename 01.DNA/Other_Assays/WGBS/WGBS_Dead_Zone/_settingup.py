9abf04f7351a23c4e49ff2d84f41396bed4f8b8f
ryansohny <phenomata@gmail.com>
2023-04-16 23:25:35 +0900

    Added wgbstools script

---
+++ 01.DNA/Other_Assays/WGBS/WGBS_Dead_Zone/_settingup.py
@@ -0,0 +1,2 @@
+import os
+os.system('wgbstools beta2bed --keep_na -c 5 --outpath ./GSM5652204_Dermal-Fibroblasts-Z00000423.hg38.bed --genome hg38 GSM5652204_Dermal-Fibroblasts-Z00000423.hg38.beta')
